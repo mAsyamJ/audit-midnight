cc1b704e62ee79db37b5c9f406f9d99d099bda7c
MathisGD <74971347+MathisGD@users.noreply.github.com>
2025-10-06 17:44:37 +0200

    test: minor improvements
    Co-authored-by: Adrien Husson <adhusson@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- test/SignatureTest.sol
+++ test/SignatureTest.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "./BaseTest.sol";
 
-uint256 constant SECP256K1 = 115792089237316195423570985008687907852837564279074904382605163141518161494337;
 
 /// @dev Ref implem from OpenZeppelin.
 function toEthSignedMessageHash(bytes32 messageHash) pure returns (bytes32 digest) {
@@ -22,8 +21,7 @@ contract SignatureTest is Test, MorphoV2 {
     }
 
     function testSigner(bytes32 root, uint256 sk) public {
-        vm.assume(sk != 0);
-        vm.assume(sk < SECP256K1);
+        sk = boundPrivateKey(sk)
         bytes32 messageHash = keccak256(bytes.concat("\x19\x45thereum Signed Message:\n32", root));
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(sk, messageHash);
         assertEq(signer(root, Signature({v: v, r: r, s: s})), vm.addr(sk));
