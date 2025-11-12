7151d732889130aeed478365b0573a7cb8c0496d
MathisGD <mathis@morpho.xyz>
2025-11-12 09:40:11 +0100

    verif: fix

--- certora/helpers/MorphoV2Helpers.sol
+++ certora/helpers/MorphoV2Helpers.sol
@@ -3,14 +3,10 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "../../src/interfaces/IERC20.sol";
-import {MorphoV2, Obligation} from "../../src/MorphoV2.sol";
+import {MorphoV2} from "../../src/MorphoV2.sol";
 
 contract MorphoV2Helpers is MorphoV2 {
     function balanceOf(address token, address account) external view returns (uint256) {
         return IERC20(token).balanceOf(account);
-    }
-
-    function id(Obligation memory obligation) external pure returns (bytes32) {
-        return _id(obligation);
     }
 }
