09b8cdaeb323808425570548e2f053f4d21874ff
MathisGD <mathis@morpho.xyz>
2026-03-02 23:47:49 +0100

    improve tests

--- test/TakeAmountsTest.sol
+++ test/TakeAmountsTest.sol
@@ -53,6 +53,13 @@ contract TakeAmountsTest is BaseTest {
         tradingFee = midnight.tradingFee(id, obligation.maturity - block.timestamp);
     }
 
+    /// @dev Returns the highest tick such that tickToPrice(tick) + tradingFee <= WAD.
+    function _maxTick(uint256 tradingFee) internal pure returns (uint256) {
+        uint256 maxPrice = WAD - tradingFee;
+        uint256 t = TickLib.priceToTick(maxPrice);
+        return TickLib.tickToPrice(t) > maxPrice ? t - 1 : t;
+    }
+
     /// @dev Creates an initial borrowing position so borrower has debt and lender has obligation shares.
     function _createPosition(uint256 positionUnits) internal returns (uint256 currentUnits, uint256 currentShares) {
         deal(address(loanToken), lender, type(uint128).max);
@@ -91,11 +98,9 @@ contract TakeAmountsTest is BaseTest {
     {
         uint256 tradingFee = _setFees(fee0, fee1);
         targetBuyerAssets = bound(targetBuyerAssets, 1, 1e30);
-        tick = bound(tick, 1, TICK_RANGE);
+        tick = bound(tick, 1, _maxTick(tradingFee));
 
         uint256 buyerPrice = TickLib.tickToPrice(tick) + tradingFee;
-        // Snap to a reachable value: not all buyerAssets are reachable when buyerPrice > WAD.
-        targetBuyerAssets = targetBuyerAssets.mulDivUp(WAD, buyerPrice).mulDivDown(buyerPrice, WAD);
         uint256 shares =
             TakeAmountsLib.buyerAssetsToShares(targetBuyerAssets, initialUnits, initialShares, buyerPrice, true);
         deal(address(loanToken), lender, type(uint256).max);
@@ -152,13 +157,11 @@ contract TakeAmountsTest is BaseTest {
     {
         uint256 tradingFee = _setFees(fee0, fee1);
         targetBuyerAssets = bound(targetBuyerAssets, 1, 1e30);
-        tick = bound(tick, 1, TICK_RANGE);
+        tick = bound(tick, 1, _maxTick(tradingFee));
 
         (uint256 currentUnits, uint256 currentShares) = _createPosition(1e36);
 
         uint256 buyerPrice = TickLib.tickToPrice(tick) + tradingFee;
-        // Snap to a reachable value: not all buyerAssets are reachable when buyerPrice > WAD.
-        targetBuyerAssets = targetBuyerAssets.mulDivUp(WAD, buyerPrice).mulDivDown(buyerPrice, WAD);
         uint256 shares =
             TakeAmountsLib.buyerAssetsToShares(targetBuyerAssets, currentUnits, currentShares, buyerPrice, false);
         deal(address(loanToken), borrower, type(uint256).max);
@@ -192,5 +195,48 @@ contract TakeAmountsTest is BaseTest {
         (, uint256 sellerAssets,,) = take(shares, borrower, offer);
 
         assertEq(sellerAssets, targetSellerAssets, "e2e sellerAssets");
+    }
+
+    // buyerPrice >= WAD: not all buyerAssets are reachable, but snapped values are.
+
+    function testSnappedBuyerAssetsBuyerIsLender(uint256 targetBuyerAssets, uint256 fee0, uint256 fee1) public {
+        uint256 tradingFee = _setFees(fee0, fee1);
+        targetBuyerAssets = bound(targetBuyerAssets, 1, 1e30);
+
+        uint256 buyerPrice = TickLib.tickToPrice(TICK_RANGE) + tradingFee;
+        uint256 targetUnits = targetBuyerAssets.mulDivUp(WAD, buyerPrice);
+
+        uint256 shares = TakeAmountsLib.unitsToShares(targetUnits, initialUnits, initialShares, true);
+        deal(address(loanToken), lender, type(uint256).max);
+        collateralize(obligation, borrower, shares.mulDivUp(initialUnits + 1, initialShares + 1));
+        offer.maker = borrower;
+        offer.tick = TICK_RANGE;
+
+        (uint256 buyerAssets,,,) = take(shares, lender, offer);
+
+        assertEq(
+            buyerAssets, targetBuyerAssets.mulDivUp(WAD, buyerPrice).mulDivDown(buyerPrice, WAD), "e2e buyerAssets"
+        );
+    }
+
+    function testSnappedBuyerAssetsBuyerIsBorrower(uint256 targetBuyerAssets, uint256 fee0, uint256 fee1) public {
+        uint256 tradingFee = _setFees(fee0, fee1);
+        targetBuyerAssets = bound(targetBuyerAssets, 1, 1e30);
+
+        (uint256 currentUnits, uint256 currentShares) = _createPosition(1e36);
+
+        uint256 buyerPrice = TickLib.tickToPrice(TICK_RANGE) + tradingFee;
+        uint256 targetUnits = targetBuyerAssets.mulDivUp(WAD, buyerPrice);
+
+        uint256 shares = TakeAmountsLib.unitsToShares(targetUnits, currentUnits, currentShares, false);
+        deal(address(loanToken), borrower, type(uint256).max);
+        offer.maker = lender;
+        offer.tick = TICK_RANGE;
+
+        (uint256 buyerAssets,,,) = take(shares, borrower, offer);
+
+        assertEq(
+            buyerAssets, targetBuyerAssets.mulDivUp(WAD, buyerPrice).mulDivDown(buyerPrice, WAD), "e2e buyerAssets"
+        );
     }
 }
