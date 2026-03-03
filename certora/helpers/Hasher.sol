2d8b7a19b391f898758c6dca4c162eb67e8f5a8b
MathisGD <mathis@morpho.xyz>
2026-03-03 15:03:23 +0100

    perf: keccak

--- certora/helpers/Hasher.sol
+++ certora/helpers/Hasher.sol
@@ -8,7 +8,7 @@ import {UtilsLib} from "../../src/libraries/UtilsLib.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/765b288a6a273f8c267adcd409094212dbd5e3cb/contracts/utils/cryptography/MerkleProof.sol#L57-L63
 contract Hasher {
     function usedKeccak256(bytes32 x, bytes32 y) external pure returns (bytes32) {
-        return keccak256(UtilsLib.sort(x, y));
+        return UtilsLib.efficientHash(x, y);
     }
 
     function commutativeKeccak256(bytes32 a, bytes32 b) public pure returns (bytes32) {
