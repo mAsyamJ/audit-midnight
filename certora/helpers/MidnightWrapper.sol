4c724e6d729bfa3610bca0e2aea90b6bd96c9f41
Jochen Hoenicke <hoenicke@gmail.com>
2026-03-23 12:54:35 +0100

    Updated to Position accounting

--- certora/helpers/MidnightWrapper.sol
+++ certora/helpers/MidnightWrapper.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import {Midnight} from "../../src/Midnight.sol";
-import {BorrowerState, Collateral, Obligation} from "../../src/interfaces/IMidnight.sol";
+import {Position, Collateral, Obligation} from "../../src/interfaces/IMidnight.sol";
 import {IOracle} from "../../src/interfaces/IOracle.sol";
 import {UtilsLib} from "../../src/libraries/UtilsLib.sol";
 import {ORACLE_PRICE_SCALE, WAD} from "../../src/libraries/ConstantsLib.sol";
@@ -15,21 +15,20 @@ contract MidnightWrapper is Midnight {
     /* This isHealthy function iterates over all collaterals, it doesn't use the collateral bitmap. */
 
     function isHealthyNoBitmap(Obligation memory obligation, bytes32 id, address borrower) public view returns (bool) {
-        BorrowerState storage _borrowerState = borrowerState[id][borrower];
-        uint256 debt = _borrowerState.debt;
+        Position storage _position = position[id][borrower];
+        uint256 debt = _position.debt;
         uint256 maxDebt;
         uint256 len = obligation.collaterals.length;
         for (uint256 i = len; i > 0 && maxDebt < debt; ) {
             i--;
             Collateral memory collateral = obligation.collaterals[i];
             uint256 price = IOracle(collateral.oracle).price();
-            maxDebt += collateralOf[id][borrower][i].mulDivDown(price, ORACLE_PRICE_SCALE)
-                .mulDivDown(collateral.lltv, WAD);
+            maxDebt += _position.collateral[i].mulDivDown(price, ORACLE_PRICE_SCALE).mulDivDown(collateral.lltv, WAD);
         }
         return maxDebt >= debt;
     }
 
     function collateralBitSet(bytes32 id, address borrower, uint256 index) external view returns (bool) {
-        return (borrowerState[id][borrower].activatedCollaterals & uint128(1 << index)) != 0;
+        return (position[id][borrower].activatedCollaterals & uint128(1 << index)) != 0;
     }
 }
