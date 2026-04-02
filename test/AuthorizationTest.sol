4afdf433aa0dc27643a58443f32bf0a7d977e076
peyha <peyha.nicole@outlook.fr>
2026-04-02 14:48:16 +0200

    feat: repay callback

--- test/AuthorizationTest.sol
+++ test/AuthorizationTest.sol
@@ -58,7 +58,7 @@ contract AuthorizationTest is BaseTest {
         skip(99);
         deal(address(loanToken), borrower, units);
         vm.prank(borrower);
-        midnight.repay(obligation, units, borrower);
+        midnight.repay(obligation, units, borrower, hex"");
 
         // Attacker tries to withdraw lender's units
         address attacker = makeAddr("attacker");
@@ -95,7 +95,7 @@ contract AuthorizationTest is BaseTest {
         skip(99);
         deal(address(loanToken), borrower, units);
         vm.prank(borrower);
-        midnight.repay(obligation, units, borrower);
+        midnight.repay(obligation, units, borrower, hex"");
 
         // Lender authorizes operator
         address operator = makeAddr("operator");
@@ -167,7 +167,7 @@ contract AuthorizationTest is BaseTest {
         skip(99);
         deal(address(loanToken), borrower, units);
         vm.prank(borrower);
-        midnight.repay(obligation, units, borrower);
+        midnight.repay(obligation, units, borrower, hex"");
 
         // Lender can withdraw their own units (no authorization needed)
         vm.prank(lender);
@@ -255,13 +255,13 @@ contract AuthorizationTest is BaseTest {
 
         vm.prank(authorized);
         vm.expectRevert("unauthorized");
-        midnight.repay(obligation, units, borrower);
+        midnight.repay(obligation, units, borrower, hex"");
 
         vm.prank(borrower);
         midnight.setIsAuthorized(borrower, authorized, true);
 
         vm.prank(authorized);
-        midnight.repay(obligation, units, borrower);
+        midnight.repay(obligation, units, borrower, hex"");
 
         assertEq(midnight.debtOf(id, borrower), 0);
     }
